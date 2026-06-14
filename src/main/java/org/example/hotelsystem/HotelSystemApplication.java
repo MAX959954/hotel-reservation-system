f5cdf97b4d678028c034e9cf9d9656a2ec6b7ba0
MAX959954 <veselmaks2504@gmail.com>
2026-06-15 01:20:09 +0200

    first commit

---
+++ src/main/java/org/example/hotelsystem/HotelSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.hotelsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelSystemApplication.class, args);
+	}
+
+}
